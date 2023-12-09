043d64376e3af608c45518033f54c3dab9ff57ce
Muz <unknown>
2023-12-09 12:19:34 +0100

    day-1 part2

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,10 +1,50 @@
 use crate::custom_error::AocError;
 
+const DIGITS: [&str; 18] = [
+    "one", "1", "two", "2", "three", "3", "four", "4",
+    "five", "5", "six", "6", "seven", "7", "eight", "8",
+    "nine", "9",
+];
+
+fn convert_number_string(str: &str) -> &str {
+    match str {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => str,
+    }
+}
+
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 2");
+    input: &str,
+) -> miette::Result<usize, AocError> {
+    Ok(input
+        .lines()
+        .map(|line| {
+            let mut a = DIGITS
+                .iter()
+                .flat_map(|digit_str| {
+                    line.match_indices(digit_str)
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<(usize, &str)>>();
+            a.sort_by(|a, b| a.0.cmp(&b.0));
+            format!(
+                "{}{}",
+                convert_number_string(a.first().unwrap().1),
+                convert_number_string(a.last().unwrap().1)
+            )
+            .parse::<usize>()
+            .unwrap()
+        })
+        .sum())
 }
 
 #[cfg(test)]
@@ -13,9 +53,14 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!(281, process(input)?);
         Ok(())
     }
 }
